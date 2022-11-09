bc0e412969f11784f14742a21ad806f67ef57440
bilal-288 <73030325+bilal-288@users.noreply.github.com>
2022-11-09 23:06:47 +0100

    login, register  and add product

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,16 +5,39 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Carbon;
 
 class UserController extends Controller
 {
+    /**
+     * I will make registration for the user
+     */
     public function register(Request $req)
     {
         $user = new User();
         $user->name = $req->input('name');
         $user->email = $req->input('email');
         $user->password = Hash::make($req->input('password'));
+        $user->created_at = Carbon::now()->toDateTimeString();
+        $user->updated_at = Carbon::now()->toDateTimeString();
         $user->save();
+
+        return response()->json(200);
         
     }
+
+    /**
+     * I will do login for the users
+     */
+
+     public function login(Request $req){
+        $user = User::where('email',$req->email)->first();
+        if(!$user || !Hash::check($req->password,$user->password) ){
+        return response(
+            [
+                'error'=> ["Some thing is wrong"]
+            ]);
+        }
+        return $user;
+     }
 }
